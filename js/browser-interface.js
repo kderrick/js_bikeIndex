f8923a2fae2d90b269a65931894683597b2ac864
Kyle Derrick and Sara Wagner <sara.wagner23q@gmail.com>
2016-03-16 16:21:49 -0700

    fix debugger and city now functional

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
     $('.showBike').html("<p>Your search results: </p>" + "<p> Brand: " + newBike.brand + "</p>" + "<p>Color: " + newBike.frameColor + "</p>" + "<p>City: " + newBike.city + "</p>" + "<p>State: " + newBike.state + "</p>");
 
-    $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&per_page=100&colors=' + newBike.frameColor + '&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20' + newBike.state + '&access_token=2snw8x3hz9tiuh9gek0lca').then(function(response) {
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?per_page=100&colors=' + newBike.frameColor +'&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20'+ newBike.state + '&access_token=2snw8x3hz9tiuh9gek0lca', function(response) {
       for(var i = 0; i < response.bikes.length; i++) {
         $(".showResult").append('<li>' + response.bikes[i].manufacturer_name + " " + response.bikes[i].stolen_location + " " + response.bikes[i].frame_colors +  '</li>');
       }
@@ -25,3 +25,6 @@ $(document).ready(function() {
 
   });
 });
+
+
+// https://bikeindex.org:443/api/v2/bikes_search?page=1&per_page=100&colors=' + newBike.frameColor + '&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20' + newBike.state +
