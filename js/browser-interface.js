bd59b2697ca5a8d0775277b0696349e90c1953bf
Kyle Derrick and Sara Wagner <kderrick1988@gmail.com>
2016-03-16 16:05:57 -0700

    fix manufacterer name error

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
 
     $('.showBike').html("<p>Your search results: </p>" + "<p> Brand: " + newBike.brand + "</p>" + "<p>Color: " + newBike.frameColor + "</p>" + "<p>City: " + newBike.city + "</p>" + "<p>State: " + newBike.state + "</p>");
 
-    $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&per_page=100&colors=' + newBike.frameColor + '&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20' + newBike.state + '&access_token=2snw8x3hz9tiuh9gek0lca', function(response) {
-      for(var i = 1; i <= response.bikes.length; i++) {
-        $(".showResult").append(response.bikes[i].manufacturer_name + " " + response.bikes[i].stolen_location + " " + response.bikes[i].frame_colors + " ");
+    $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&per_page=100&colors=' + newBike.frameColor + '&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20' + newBike.state + '&access_token=2snw8x3hz9tiuh9gek0lca').then(function(response) {
+      for(var i = 0; i < response.bikes.length; i++) {
+        $(".showResult").append('<li>' + response.bikes[i].manufacturer_name + " " + response.bikes[i].stolen_location + " " + response.bikes[i].frame_colors +  '</li>');
       }
       // console.log(response.bikes);
       // $(".showResult").text(response.bikes);
