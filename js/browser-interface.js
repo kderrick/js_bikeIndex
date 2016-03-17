ad826633bb56fc4d78605aeb23baca21a0e3e3d9
Kyle Derrick and Sara Wagner <sara.wagner23q@gmail.com>
2016-03-17 15:25:58 -0700

    add 1 year search date query

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -2,6 +2,9 @@ var Bike = require('./../js/bike.js').Bike;
 
 $(document).ready(function() {
   $('#buttonSubmit').click(function() {
+    var searchDate = Date.now();
+    var dateWithinAYear = (searchDate/1000) - 31536000;
+
     var searchCity = $('#searchCity').val();
     var searchState = $('#searchState').val();
     var searchBrand = $('#searchBrand').val();
@@ -12,15 +15,16 @@ $(document).ready(function() {
     $('#searchBrand').val("");
     $('#searchColor').val("");
 
+    $(".showResult").empty();
     $('.showBike').html("<p>Your search results: </p>" + "<p> Brand: " + newBike.brand + "</p>" + "<p>Color: " + newBike.frameColor + "</p>" + "<p>City: " + newBike.city + "</p>" + "<p>State: " + newBike.state + "</p>");
 
-    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?per_page=100&colors=' + newBike.frameColor +'&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20'+ newBike.state + '&access_token=2snw8x3hz9tiuh9gek0lca', function(response) {
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?per_page=100&colors=' + newBike.frameColor +'&manufacturer=' + newBike.brand + '&proximity=' + newBike.city + '%2C%20'+ newBike.state + "&stolen_after=" + dateWithinAYear, function(response) {
       for(var i = 0; i < response.bikes.length; i++) {
         $(".showResult").append('<li>' + response.bikes[i].manufacturer_name + " " + response.bikes[i].stolen_location + " " + response.bikes[i].frame_colors +  '</li>');
       }
       // console.log(response.bikes);
       // $(".showResult").text(response.bikes);
-
+      $('.numberOfResults').text(response.bikes.length + " Bikes returned");
     });
 
   });
